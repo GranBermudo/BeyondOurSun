be269e4112db5e004d13a1a28456ac386901150d
ZORG\thomas <thomas.valentin.garrel@gmail.com>
2022-01-31 17:26:47 +0100

    Systeme de pv ajouter Les ennemis nous poursuive selon une distance Debtu de systeme de mission Ajout du strafe sur le joystick droit

---
+++ Beyond_Our_Sun/Assets/Scripts/EnnemisIA.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemisIA : MonoBehaviour
+{
+
+    private Transform player;
+    public float distanceDeDetection;
+    public float vitesse;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform; //chopper une ref a la liste des cibles, y a surement une alternative au FindGameobject a voir avec Bruno
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, player.position) > distanceDeDetection) 
+		{
+
+            //patrouille
+		}
+        else
+		{
+            //poursuite
+            transform.LookAt(player.position);
+            transform.Translate(Vector3.forward * vitesse * Time.deltaTime); //Possibilite d'augmenter la vitesse pour ennemi kamikaze
+		}
+    }
+}
