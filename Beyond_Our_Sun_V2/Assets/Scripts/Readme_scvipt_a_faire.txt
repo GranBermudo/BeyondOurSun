f1baaa5bc414da7414d05ba4277aaccc3a4f3dca
ZORG\thomas <thomas.valentin.garrel@gmail.com>
2022-02-09 00:53:28 +0100

    Modif sur l'UI

--- Beyond_Our_Sun_V2/Assets/Scripts/Readme_scvipt_a_faire.txt
+++ Beyond_Our_Sun_V2/Assets/Scripts/Readme_scvipt_a_faire.txt
@@ -8,3 +8,14 @@ Retourne la caméra ou la switch pour voir derrière avec un bouton
 Script lors des debuts de mission qui met en contexte ce qu'on va faire avec les ojectif met le jeu en pose 
 En jeu dialogue qui s'active lorsqu'on effectue une action
 
+Mise en scene
+
+tableau de string avec les replique dedans
+Int = indexDialogue
+ 
+a chaque clic on augmenter l'indexDialogue 
+j'affiche dans le text la valeur de la du case du tableau du correspondante (a indexDialogue)
+
+Si indexDialogue >= a la taille du tableau => fin du dialogue
+
+Pour avoir les images des personnes remplacer les string par des structures (composé de string avec reference a l'image afficher et possible un deuxieme string avec le nom par exemple)
--- Beyond_Our_Sun_V2/Assets/TOMA/UI/LevelLoader.cs
+++ Beyond_Our_Sun_V2/Assets/TOMA/UI/LevelLoader.cs
@@ -1,4 +1,4 @@
-﻿/*using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,31 +6,60 @@ using UnityEngine.UI;
 
 public class LevelLoader : MonoBehaviour
 {
-    public void LoadLevel (int sceneIndex)
+	public GameObject loadingScreen;
+	public GameObject BoutonMenuControlles;
+	//public GameObject MenuControlles;
+	public Slider slider;
+	public Text ProgressText;
+	public bool isloadingScreenActive;
+	public bool MenuControllesActive;
+		
+
+
+	public void LoadLevel(int sceneIndex)
 	{
 		//AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
-	StartCoroutine(LoadAsynchronously(sceneIndex));
-
-	public GameObject loadingScreen;
-    public Slider slider;
-	public Text ProgressText;
+		StartCoroutine(LoadAsynchronously(sceneIndex));
 
 	}
 
-	IEnumerator LoadAsynchronously (int sceneIndex)
-	{
-	AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+	
+	
 
-	loadingScreen.SetActive(true);
+	IEnumerator LoadAsynchronously(int sceneIndex)
+	{
+		AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+		loadingScreen.SetActive(true);
+
+		isloadingScreenActive = true;
+
+		MenuControllesActive = false;
+
 
 		while (!operation.isDone)
 		{
-	    float progress = Mathf.Clamp01(operation.progress / .9f);
-		slider.value = progress;
-		ProgressText.text = progress * 100f + "%";
+			float progress = Mathf.Clamp01(operation.progress / .9f);
+			slider.value = progress;
+			ProgressText.text = progress * 100f + "%";
 
-		yield return null;
+			yield return null;
 		}
 	}
+
+	void Update()
+	{
+		if (isloadingScreenActive == true)
+			BoutonMenuControlles.SetActive(false);
+		if (isloadingScreenActive == false)
+			BoutonMenuControlles.SetActive(true);
+
+		if (MenuControllesActive == true)
+			BoutonMenuControlles.SetActive(false);
+		if (MenuControllesActive == false)
+			BoutonMenuControlles.SetActive(true);
+
+	}
+
+
 }
-*/
